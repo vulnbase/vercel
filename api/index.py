5633ba66117b98f601e59cdfa74f11ef35ac9817
vulnbase <40605654+vulnbase@users.noreply.github.com>
2024-02-16 09:41:58 +0000

    iframe

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type','text/html')
         self.end_headers()
-        self.wfile.write('Hello, world 2!'.encode('utf-8'))
+        self.wfile.write('<html><body><h1>Hello, iframe!</h1><iframe src=https://vercel.webhooks.pw/index.html></iframe></body></html>'.encode('utf-8'))
         return

